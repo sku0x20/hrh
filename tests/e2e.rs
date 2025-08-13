9da1927666c6b76527b4b1a87bc3e970a772bf23
siddhant <siddhantkumarupmanyu@gmail.com>
2025-08-13 17:03:21 +0530

    Enhance E2E test to execute binary and verify successful execution status

--- tests/e2e.rs
+++ tests/e2e.rs
@@ -1,7 +1,16 @@
 use std::env;
+use std::process::Command;
+use std::process::Stdio;
 
 #[test]
 fn e2e() {
     let binary_path = env::args().nth(2).unwrap();
-    assert_eq!(binary_path, "target/debug/hrh");
+
+    let output = Command::new(binary_path)
+        .stdout(Stdio::piped())
+        .stderr(Stdio::piped())
+        .output()
+        .expect("failed to execute process");
+
+    assert!(output.status.success());
 }
