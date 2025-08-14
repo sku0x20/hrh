1a2fe9ebe2eb490abc42727591e20aae82689141
siddhant <siddhantkumarupmanyu@gmail.com>
2025-08-14 13:43:12 +0530

    failing e2e

--- tests/e2e.rs
+++ tests/e2e.rs
@@ -1,17 +1,25 @@
-use std::env;
+use std::fs::File;
+use std::io::Read;
 use std::process::Command;
 use std::process::Stdio;
+use std::{env, fs};
+
+const OUTPUT_FILE: &str = "fake_helm.out";
 
 #[test]
 fn e2e() {
     let binary_path = env::args().last().unwrap();
+
+    fs::remove_file(OUTPUT_FILE).unwrap_or_else(|_| {
+        eprintln!("failed to remove fake_helm.out");
+    });
 
     let output = Command::new(binary_path)
         .args([
             "-f",
             "tests/resources/vm-agent.yaml",
             "--helm-path",
-            "fake_helm.sh",
+            "./fake_helm.sh",
         ])
         .stdout(Stdio::piped())
         .stderr(Stdio::piped())
@@ -20,4 +28,15 @@ fn e2e() {
 
     assert!(output.status.success());
     println!("{:#?}", output);
+    let mut fake_helm_out =
+        File::open(OUTPUT_FILE).expect(&format!("failed to open {}", OUTPUT_FILE));
+    let mut output = String::new();
+    fake_helm_out
+        .read_to_string(&mut output)
+        .expect(&format!("failed to open {}", OUTPUT_FILE));
+
+    assert_eq!(
+        output,
+        "./fake_helm.sh upgrade --install pod-collector vm/victoria-metrics-agent --namespace observability -f pod-collector.yaml\n"
+    );
 }
