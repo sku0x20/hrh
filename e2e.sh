55d9eb4d4e0b4c5ac969d883023133054cc8e4ca
siddhant <siddhantkumarupmanyu@gmail.com>
2025-08-14 10:06:41 +0530

    Enhance E2E test to capture output and add CLI arguments for binary execution

--- e2e.sh
+++ e2e.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
 cargo build
-cargo test --test e2e -- e2e "target/debug/hrh"
+cargo test --test e2e -- --nocapture e2e "target/debug/hrh"
--- tests/e2e.rs
+++ tests/e2e.rs
@@ -4,13 +4,15 @@ use std::process::Stdio;
 
 #[test]
 fn e2e() {
-    let binary_path = env::args().nth(2).unwrap();
+    let binary_path = env::args().last().unwrap();
 
     let output = Command::new(binary_path)
+        .args(["-f", "tests/resources/vm-agent.yaml", "--helm-path", "fake_helm.sh"])
         .stdout(Stdio::piped())
         .stderr(Stdio::piped())
         .output()
         .expect("failed to execute process");
 
     assert!(output.status.success());
+    println!("{:?}", output);
 }
