ce7da4fa4c2a5964c362cd7cd1ddc85115bfeba7
siddhant <siddhantkumarupmanyu@gmail.com>
2025-08-14 14:31:21 +0530

    Add `execute_helm` function to run helm commands in `run` function

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,17 @@
+use std::process::{Command, Stdio};
+
 pub mod config;
 
 pub fn run(config: config::Config) {
     println!("{:?}", config);
+    execute_helm(config.helm_path);
+}
+
+fn execute_helm(helm: String) {
+    let output = Command::new(helm)
+        .args(["--something", "tests/resources.out"])
+        .stdout(Stdio::piped())
+        .stderr(Stdio::piped())
+        .output()
+        .expect("failed to execute helm");
 }
